077e0968385e412a7cf870e305d0d1955e159d3c
ngc7331 <ngc7331@outlook.com>
2024-12-01 00:13:41 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=${VERSION:-3.20}
+REPOS=(${REPOS:-ngc7331/linuxserver-baseimage-alpine})
+OFFICIAL_REPO=lscr.io/linuxserver/baseimage-alpine
+RISCV_REPO=ghcr.io/unofficial-docker-for-riscv/linuxserver-baseimage-alpine
+
+echo "Building version: ${VERSION}"
+echo "Building repos: ${REPOS[*]}"
+
+echo "Combining with images..."
+TAGS=()
+for repo in ${REPOS[@]}; do
+    TAGS+=("-t ${repo}:${VERSION}")
+    TAGS+=("-t ${repo}:${VERSION%.*}")
+    TAGS+=("-t ${repo}:latest")
+done
+docker buildx imagetools create \
+    ${OFFICIAL_REPO}:${VERSION} \
+    ${RISCV_REPO}:${VERSION} \
+    ${TAGS[*]}
