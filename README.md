719190c07b8c2598486d1caab976197314e59fe1
ngc7331 <ngc7331@outlook.com>
2025-03-05 10:19:21 +0800

    chore: auto update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Simple repo to combine [Linuxserver official amd64/arm64 docker-baseimage-alpine image](https://github.com/linuxserver/docker-baseimage-alpine) with [unofficial riscv64 image](https://github.com/unofficial-docker-for-riscv/linuxserver-baseimage-alpine) for my multi-arch image builds.
+
+# Last Build Info
+- Alpine 3.21 upstream sha: null
+- Alpine 3.21 RISCV64 sha: null
--- build.sh
+++ build.sh
@@ -10,27 +10,6 @@ RISCV_REPO=ghcr.io/unofficial-docker-for-riscv/linuxserver-baseimage-alpine
 echo "Building version: ${VERSION}"
 echo "Building repos: ${REPOS[*]}"
 
-# Check update
-need_update=false
-checkupdate() {
-    repo=$1
-    arch=$2
-    echo "Checking update for ${arch}..."
-    oldsha=$(docker buildx imagetools inspect --raw ${REPOS[0]}:${VERSION} | jq -r '.manifests[] | select(.platform.architecture == "'${arch}'") | .digest')
-    echo "-> Old SHA (${REPOS[0]}:${VERSION}): ${oldsha}"
-    newsha=$(docker buildx imagetools inspect --raw ${repo}:${VERSION} | jq -r '.manifests[] | select(.platform.architecture == "'${arch}'") | .digest')
-    echo "-> New SHA (${repo}:${VERSION}): ${newsha}"
-    if [ "${oldsha}" != "${newsha}" ]; then
-        need_update=true
-    fi
-}
-checkupdate ${OFFICIAL_REPO} amd64  # we don't have to check arm64
-checkupdate ${RISCV_REPO} riscv64
-if [[ "$need_update" == false ]]; then
-    echo "No update needed"
-    exit 0
-fi
-
 echo "Combining newer images..."
 TAGS=()
 for repo in ${REPOS[@]}; do
